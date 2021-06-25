c657bd832834c99386232237643324d779e17e91
Roman <r.raekow@gmail.com>
2021-06-25 23:59:12 +0200

    inital

---
+++ build/CMakeFiles/samplesToFile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/samplesToFile.dir/samplesToFile.cpp.o"
+  "samplesToFile"
+  "samplesToFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/samplesToFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
